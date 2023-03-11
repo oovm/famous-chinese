47a4f9bef455266e8004412c7bbaf1710ebd519a
Aster <galaster@foxmail.com>
2023-03-11 22:32:59 +0800

    Find tag in categories

--- projects/famous-chinese/src/main.rs
+++ projects/famous-chinese/src/main.rs
@@ -1,106 +1,62 @@
 #![feature(once_cell)]
 
+use std::collections::BTreeSet;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::iter::Peekable;
 use std::path::Path;
 use std::sync::LazyLock;
 
 use itertools::{Itertools, PeekingNext};
 use regex::Regex;
-use utf8_chars::{BufReadCharsExt, Chars};
+use tl::queryselector::iterable::QueryIterable;
+use utf8_chars::BufReadCharsExt;
+
+pub use crate::pair_tags::CaptureTag;
 
 mod errors;
 
-// [[Category:1955年啟用的鐵路車站]]
-pub static CATEGORY_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"\[\[Category:([^]]+)]]").unwrap());
+mod pair_tags;
 
+// [[Category:1955年啟用的鐵路車站]]
+pub static CATEGORY_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"(?:\[\[)Category:([^]]+)(?:]])").unwrap());
+
+pub static TITLE_REGEX: LazyLock<Regex> = LazyLock::new(|| Regex::new(r"(?:<title>)([^<]+)(?:</title>)").unwrap());
 
 fn main() {
     let here = Path::new(env!("CARGO_MANIFEST_DIR")).join("../wikipedia").canonicalize().unwrap();
     let data = here.join("zhwiki-20230301-pages-articles-multistream.xml");
     let mut reader = BufReader::new(File::open(data).unwrap());
     let mut stream = CaptureTag::new(&mut reader, "page").unwrap();
-    for page in stream.take(1) {
-        println!("{}", page.text);
+    for page in stream {
+        match Page::build(page) {
+            None => {}
+            Some(s) => {
+                if s.categories.contains("华人") {
+                    println!("{}", s.title);
+                    break
+                }
+            }
+        }
+    }
+}
+
+impl Page {
+    pub fn build<S: Into<String>>(text: S) -> Option<Self> {
+        let page = text.into();
+        let title = TITLE_REGEX.captures(&page)?.get(1)?.as_str().to_string();
+        let mut categories = BTreeSet::default();
+        for item in CATEGORY_REGEX.captures_iter(&page) {
+            categories.insert(item.get(1)?.as_str().to_string());
+        }
+        Some(Self {
+            title,
+            categories,
+        })
     }
 }
 
 
 pub struct Page {
-    pub text: String,
-}
-
-pub struct CaptureTag<'i>
-{
-    pattern: String,
-    buffer: String,
-    reader: Peekable<Chars<'i, BufReader<File>>>,
-}
-
-impl<'i> CaptureTag<'i> {
-    pub fn new<S: Into<String>>(br: &'i mut BufReader<File>, tag: S) -> std::io::Result<Self> {
-        let reader = br.chars().peekable();
-        Ok(Self {
-            pattern: tag.into(),
-            buffer: String::new(),
-            reader,
-        })
-    }
-    fn peek_tag(&mut self) -> String {
-        let mut peek_buffer = String::new();
-        // peek until >
-        for item in self.reader.peeking_take_while(end_peek) {
-            match item {
-                Ok(c) => {
-                    peek_buffer.push(c)
-                }
-                Err(_) => {}
-            }
-        }
-        if !peek_buffer.is_empty() {
-            println!("peek: {}", peek_buffer)
-        }
-        peek_buffer
-    }
-}
-
-fn end_peek(c: &std::io::Result<char>) -> bool {
-    match c {
-        Ok(c) => '>'.ne(c),
-        Err(_) => false,
-    }
-}
-
-impl<'i> Iterator for CaptureTag<'i> {
-    type Item = Page;
-    // <page> ... </page>
-    fn next(&mut self) -> Option<Self::Item> {
-        let mut text_buffer = String::new();
-        let mut in_page = false;
-        while let Some(c) = self.reader.next() {
-            match c {
-                // <tag>
-                Ok('<') if !in_page => {
-                    if self.peek_tag().eq(&self.pattern) {
-                        in_page = true;
-                    }
-                }
-                // </tag>
-                Ok('<') if in_page => {
-                    if self.peek_tag().eq(&format!("/{}", self.pattern)) {
-                        in_page = false;
-                        return Some(Page { text: text_buffer });
-                    }
-                }
-                Ok(c) if in_page => {
-                    text_buffer.push(c);
-                }
-                _ => {
-                    continue;
-                }
-            }
-        }
-        None
-    }
+    pub title: String,
+    pub categories: BTreeSet<String>,
 }
---
+++ projects/famous-chinese/src/pair_tags.rs
@@ -0,0 +1,99 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::iter::Peekable;
+
+use itertools::Itertools;
+use utf8_chars::{BufReadCharsExt, Chars};
+
+pub struct CaptureTag<'i>
+{
+    pattern: String,
+    buffer: String,
+    reader: Peekable<Chars<'i, BufReader<File>>>,
+}
+
+impl<'i> CaptureTag<'i> {
+    pub fn new<S: Into<String>>(br: &'i mut BufReader<File>, tag: S) -> std::io::Result<Self> {
+        let reader = br.chars().peekable();
+        Ok(Self {
+            pattern: tag.into(),
+            buffer: String::new(),
+            reader,
+        })
+    }
+
+    fn is_start_pattern(&self, s: &str) -> bool {
+        format!("<{}", self.pattern).eq(s)
+    }
+    fn is_end_pattern(&self, s: &str) -> bool {
+        format!("</{}", self.pattern).eq(s)
+    }
+    // peek until >
+    fn peek_tag(&mut self) -> String {
+        let mut peek_buffer = "<".to_string();
+        for item in self.reader.peeking_take_while(continue_peek) {
+            match item {
+                Ok(c) => {
+                    peek_buffer.push(c)
+                }
+                Err(_) => {
+                    break;
+                }
+            }
+        }
+        peek_buffer
+    }
+}
+
+fn continue_peek(c: &std::io::Result<char>) -> bool {
+    match c {
+        Ok(c) => '>'.ne(c),
+        Err(_) => false,
+    }
+}
+
+impl<'i> Iterator for CaptureTag<'i> {
+    type Item = String;
+    /// `<tag> ... </tag>`
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut text_buffer = String::new();
+        let mut in_page = false;
+        while let Some(c) = self.reader.next() {
+            match c {
+                // <tag>
+                Ok('<') if !in_page => {
+                    match self.peek_tag().as_str() {
+                        s if self.is_start_pattern(s) => {
+                            in_page = true;
+                            text_buffer.push_str(s);
+                        }
+                        _ => continue,
+                    }
+                }
+                // </tag>
+                Ok('<') if in_page => {
+                    match self.peek_tag().as_str() {
+                        s if self.is_end_pattern(s) => {
+                            in_page = false;
+                            text_buffer.push_str(s);
+                            text_buffer.push('>');
+                            return Some(text_buffer);
+                        }
+                        s => text_buffer.push_str(s),
+                    }
+                }
+                Ok(c) if in_page => {
+                    text_buffer.push(c);
+                }
+                _ => {
+                    continue;
+                }
+            }
+        }
+        if text_buffer.is_empty() {
+            None
+        } else {
+            Some(text_buffer)
+        }
+    }
+}
